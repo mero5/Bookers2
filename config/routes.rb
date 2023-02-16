c8f017abc3cfa240905121de40dac46bd6e36700
mero5 <kksw-2824@outlook.jp>
2023-02-16 07:00:11 +0000

    first1-1

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/new'
+  devise_for :users
+  root to: "homes#top"
+  get 'homes/about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+end
