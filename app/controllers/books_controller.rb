2c3b8e8b87baac0cd59c45776347963f03fd5f7d
mero5 <kksw-2824@outlook.jp>
2023-02-18 09:53:50 +0000

    1-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,17 @@
 class BooksController < ApplicationController
   def index
+    #左下book/新規作成
     @book = Book.new
+    #左上プロフィール
+    @user = current_user
     @books = Book.all
   end
 
   def show
+    #左下book/新規作成
+    @book = Book.new
+    #左上プロフィール
+    @user = current_user
     @book = Book.find(params[:id])
   end
 
@@ -15,13 +22,22 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to book_path(@book.id)
   end
   
   def deatroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def index
+    #左下book/新規作成
+    @book = Book.new
+    #左上プロフィール
+    @user = current_user
+    #全てのusersを出力
     @users = User.all
   end
 
   def show
+    #左下book/新規作成
+    @book = Book.new
+    #左上プロフィール
     @user = User.find(params[:id])
+    #上のuserだけのbooks
     @books = @user.books
   end
 
@@ -21,6 +30,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:title, :body, :image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
