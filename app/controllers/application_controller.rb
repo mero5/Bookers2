de047c5694188c97532bd23fccea074d0ee32ac6
EC2 Default User <ec2-user@ip-172-31-0-165.ap-northeast-1.compute.internal>
2023-02-18 18:34:28 +0000

    1-4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,13 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def after_sign_in_path_for(resource)
-    users_path
+    flash[:notice] = "Welcome! You have signed up successfully."
+    user = current_user
+    user_path(user.id)
   end
 
   def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
     root_path
   end
 end
