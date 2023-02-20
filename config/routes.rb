d259537d3efab93acaa1321cd81df87381536a29
mero5 <kksw-2824@outlook.jp>
2023-02-20 09:27:49 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   resources :users,only: [:index, :show, :edit, :update, :deatroy]
   resources :books,only: [:index, :show, :edit, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
