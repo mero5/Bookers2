493d4ddeee00ddd3444510ea46aeda8a9ceb6be4
EC2 Default User <ec2-user@ip-172-31-0-165.ap-northeast-1.compute.internal>
2023-03-02 03:25:19 +0000

    2-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
   def index
     #左下book/新規作成
     @book = Book.new
