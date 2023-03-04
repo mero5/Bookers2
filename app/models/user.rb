8a53e6e6b7163bb6ce53e2ff40760a09d194f399
EC2 Default User <ec2-user@ip-172-31-0-165.ap-northeast-1.compute.internal>
2023-03-04 05:23:30 +0000

    1-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,28 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  #あるユーザーがフォローしてる人（フォロワー）をとってくる
-  has_many :followings, through: :relationships, source: :followed
 
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+  # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
-  
   # フォローしたときの処理
-def follow(user_id)
-  relationships.create(followed_id: user_id)
-end
-# フォローを外すときの処理
-def unfollow(user_id)
-  relationships.find_by(followed_id: user_id).destroy
-end
-# フォローしているか判定
-def following?(user)
-  followings.include?(user)
-end
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
 
   has_one_attached :profile_image
 
