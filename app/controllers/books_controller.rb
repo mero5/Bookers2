baf89c8b8d2bf09f5e4515dd98983355e47ae5b6
EC2 Default User <ec2-user@ip-172-31-0-165.ap-northeast-1.compute.internal>
2023-03-06 15:50:43 +0000

    6-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,40 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
-  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
-  
-  def index
-    #左下book/新規作成
-    @book = Book.new
-    #左上プロフィール
-    @user = current_user
-    #すべてのbooks
-    @books = Book.all
-    #bookのを投稿したuser
-  end
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
-    #左下book/新規作成
-    @book_new = Book.new
     @book = Book.find(params[:id])
-    #左上プロフィール、bookを投稿した人
-    @user = @book.user
     @book_comment = BookComment.new
   end
 
-  def edit
-    @book = Book.find(params[:id])
+  def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      @user = current_user
-      render :index
+      render 'index'
     end
   end
 
+  def edit
+  end
+
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render :edit
+      render "edit"
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -59,11 +44,10 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-  def is_matching_login_user
+
+  def ensure_correct_user
     @book = Book.find(params[:id])
-    #unless bookを投稿したuserのid == ログインしているuserのid
-    unless @book.user_id == current_user.id
+    unless @book.user == current_user
       redirect_to books_path
     end
   end
