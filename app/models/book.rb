5617781453edd9da311bd221bfeffdfb636c4059
EC2 Default User <ec2-user@ip-172-31-0-165.ap-northeast-1.compute.internal>
2023-03-11 05:58:13 +0000

    9a

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+  has_many :view_counts, dependent: :destroy
 
   has_many :week_favorites, -> {
     where(created_at: ((Time.current.at_end_of_day - 6.day)
